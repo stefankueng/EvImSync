df87ba927262f45f8e46155290f1cf347cc15dc7
steveking <steveking@b5fa89cc-b1ba-491c-ba8b-bf438c492d4e>
2010-09-05 18:26:02 +0000

    * extend the commands to allow sending data after the initial command * add the APPEND command and get it working

---
+++ src/InterIMAP/InterIMAP-Async/InterIMAP/Common/Requests/AppendRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InterIMAP.Common.Commands;
+using InterIMAP.Common.Interfaces;
+using InterIMAP.Common.Processors;
+
+namespace InterIMAP.Common.Requests
+{
+    public class AppendRequest : BaseRequest
+    {
+        public AppendRequest(string emlContent, string flags, IFolder parentFolder, RequestCompletedCallback callback)
+            : base(callback)
+        {
+            Command = new AppendCommand(emlContent, flags, parentFolder, null);
+            ProcessorType = typeof (AppendProcessor);
+        }
+    }
+}

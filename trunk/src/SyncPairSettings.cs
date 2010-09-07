1e2d2a09ec4a02d0a28f0f1b4e6c2de2e51fceac
steveking <steveking@b5fa89cc-b1ba-491c-ba8b-bf438c492d4e>
2010-09-07 19:32:08 +0000

    Add configuration class.

---
+++ trunk/src/SyncPairSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace EveImSync
+{
+    [Serializable]
+    public class SyncPairSettings
+    {
+        public SyncPairSettings()
+        {
+
+        }
+        public string EvernoteNotebook = string.Empty;
+        public string IMAPUsername = string.Empty;
+        public string IMAPPassword = string.Empty;
+        public string IMAPServer = string.Empty;
+        public string IMAPNotesFolder = string.Empty;
+    }
+}

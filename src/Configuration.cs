d0a5870dd5707c21bc3c53986307cfea451d632c
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2010-09-07 19:32:08 +0000

    Add configuration class.

---
+++ src/Configuration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace EveImSync
+{
+    [Serializable]
+    [XmlRootAttribute("Configuration", Namespace = "", IsNullable = false)]
+    public class Configuration
+    {
+        private Configuration()
+        {
+        }
+
+        static public Configuration Create()
+        {
+            Configuration c = new Configuration();
+            XmlSerializer xs = new XmlSerializer(typeof(Configuration));
+            using (FileStream fs = File.OpenRead(@"D:\Development\EvImSync\config.xml"))
+            {
+                c = (Configuration)xs.Deserialize(fs);
+            }
+
+            return c;
+        }
+
+        public void Save()
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(Configuration));
+            using (FileStream fs = File.Create(@"D:\Development\EvImSync\config.xml"))
+            {
+                xs.Serialize(fs, this);
+            }
+        }
+
+        public List<SyncPairSettings> syncPairs = new List<SyncPairSettings>();
+
+        public string ENScriptPath = string.Empty;
+    }
+}

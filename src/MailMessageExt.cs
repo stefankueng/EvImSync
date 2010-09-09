00443da82e43591b273532b57c9de7cf2fb3aa21
steveking <steveking@b5fa89cc-b1ba-491c-ba8b-bf438c492d4e>
2010-09-09 19:09:35 +0000

    Adjust for StyleCop rules.

--- src/MailMessageExt.cs
+++ src/MailMessageExt.cs
@@ -1,66 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// EvImSync - A tool to sync Evernote notes to IMAP mails and vice versa
+// Copyright (C) 2010 - Stefan Kueng
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.IO;
 using System.Net.Mail;
 using System.Reflection;
-using System.IO;
+using System.Text;
 
 namespace EveImSync
 {
+    /// <summary>
+    /// Extension class to extend the MailMessage
+    /// </summary>
     public static class MailMessageExt
     {
-        public static string GetEmailAsString(this MailMessage Message)
+        /// <summary>
+        /// Returns the email message as a string in eml format
+        /// </summary>
+        /// <param name="message">the MailMessage object</param>
+        /// <returns>the eml string</returns>
+        public static string GetEmailAsString(this MailMessage message)
         {
             string email = null;
             Assembly assembly = typeof(SmtpClient).Assembly;
-            Type _mailWriterType =
+            Type mailWriterType =
               assembly.GetType("System.Net.Mail.MailWriter");
 
-            using (MemoryStream _stringStream =
+            using (MemoryStream stringStream =
                    new MemoryStream())
             {
                 // Get reflection info for MailWriter constructor
-                ConstructorInfo _mailWriterContructor =
-                    _mailWriterType.GetConstructor(
+                ConstructorInfo mailWriterContructor =
+                    mailWriterType.GetConstructor(
                         BindingFlags.Instance | BindingFlags.NonPublic,
                         null,
                         new Type[] { typeof(Stream) },
                         null);
 
                 // Construct MailWriter object with our FileStream
-                object _mailWriter =
-                  _mailWriterContructor.Invoke(new object[] { _stringStream });
+                object mailWriter =
+                  mailWriterContructor.Invoke(new object[] { stringStream });
 
                 // Get reflection info for Send() method on MailMessage
-                MethodInfo _sendMethod =
+                MethodInfo sendMethod =
                     typeof(MailMessage).GetMethod(
                         "Send",
                         BindingFlags.Instance | BindingFlags.NonPublic);
 
                 // Call method passing in MailWriter
-                _sendMethod.Invoke(
-                    Message,
+                sendMethod.Invoke(
+                    message,
                     BindingFlags.Instance | BindingFlags.NonPublic,
                     null,
-                    new object[] { _mailWriter, true },
+                    new object[] { mailWriter, true },
                     null);
 
                 // Finally get reflection info for Close() method on our MailWriter
-                MethodInfo _closeMethod =
-                    _mailWriter.GetType().GetMethod(
+                MethodInfo closeMethod =
+                    mailWriter.GetType().GetMethod(
                         "Close",
                         BindingFlags.Instance | BindingFlags.NonPublic);
 
                 // Call close method
-                _closeMethod.Invoke(
-                    _mailWriter,
+                closeMethod.Invoke(
+                    mailWriter,
                     BindingFlags.Instance | BindingFlags.NonPublic,
                     null,
                     new object[] { },
                     null);
 
-                email = Encoding.UTF8.GetString(_stringStream.ToArray());
+                email = Encoding.UTF8.GetString(stringStream.ToArray());
             }
+
             return email;
         }
     }

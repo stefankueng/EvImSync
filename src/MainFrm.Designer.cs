520ee084c3e23917f2a0813e1ae8e8d53df17184
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2010-09-10 18:28:19 +0000

    Add a configuration dialog.

--- src/MainFrm.Designer.cs
+++ src/MainFrm.Designer.cs
@@ -52,17 +52,7 @@ namespace EveImSync
             this.startsync = new System.Windows.Forms.Button();
             this.progressInfoList = new System.Windows.Forms.ListView();
             this.column1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.userName = new System.Windows.Forms.TextBox();
-            this.notesFolder = new System.Windows.Forms.TextBox();
-            this.passWord = new System.Windows.Forms.TextBox();
-            this.noteBook = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -89,6 +79,7 @@ namespace EveImSync
             this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
             this.optionsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
             this.optionsToolStripMenuItem.Text = "Options";
+            this.optionsToolStripMenuItem.Click += new System.EventHandler(this.OptionsToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
@@ -120,99 +111,16 @@ namespace EveImSync
             this.progressInfoList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
             this.progressInfoList.Location = new System.Drawing.Point(12, 56);
             this.progressInfoList.Name = "progressInfoList";
-            this.progressInfoList.Size = new System.Drawing.Size(497, 253);
+            this.progressInfoList.Size = new System.Drawing.Size(497, 245);
             this.progressInfoList.TabIndex = 2;
             this.progressInfoList.UseCompatibleStateImageBehavior = false;
             this.progressInfoList.View = System.Windows.Forms.View.Details;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.noteBook);
-            this.groupBox1.Controls.Add(this.passWord);
-            this.groupBox1.Controls.Add(this.notesFolder);
-            this.groupBox1.Controls.Add(this.userName);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 323);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(496, 81);
-            this.groupBox1.TabIndex = 3;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Config";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Username:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(198, 16);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Password:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 39);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(57, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Notebook:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(198, 39);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(99, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "IMAP Notes Folder:";
-            // 
-            // userName
-            // 
-            this.userName.Location = new System.Drawing.Point(70, 13);
-            this.userName.Name = "userName";
-            this.userName.Size = new System.Drawing.Size(100, 20);
-            this.userName.TabIndex = 4;
-            // 
-            // notesFolder
-            // 
-            this.notesFolder.Location = new System.Drawing.Point(303, 32);
-            this.notesFolder.Name = "notesFolder";
-            this.notesFolder.Size = new System.Drawing.Size(100, 20);
-            this.notesFolder.TabIndex = 5;
-            // 
-            // passWord
-            // 
-            this.passWord.Location = new System.Drawing.Point(303, 9);
-            this.passWord.Name = "passWord";
-            this.passWord.PasswordChar = '*';
-            this.passWord.Size = new System.Drawing.Size(100, 20);
-            this.passWord.TabIndex = 6;
-            // 
-            // noteBook
-            // 
-            this.noteBook.Location = new System.Drawing.Point(70, 36);
-            this.noteBook.Name = "noteBook";
-            this.noteBook.Size = new System.Drawing.Size(100, 20);
-            this.noteBook.TabIndex = 7;
             // 
             // MainFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(521, 410);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(521, 313);
             this.Controls.Add(this.progressInfoList);
             this.Controls.Add(this.startsync);
             this.Controls.Add(this.menuStrip1);
@@ -221,8 +129,6 @@ namespace EveImSync
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -237,15 +143,6 @@ namespace EveImSync
         private System.Windows.Forms.Button startsync;
         private System.Windows.Forms.ListView progressInfoList;
         private System.Windows.Forms.ColumnHeader column1;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox noteBook;
-        private System.Windows.Forms.TextBox passWord;
-        private System.Windows.Forms.TextBox notesFolder;
-        private System.Windows.Forms.TextBox userName;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
     }
 }
 

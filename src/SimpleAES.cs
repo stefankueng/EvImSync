49026d00c5f93359372004a1b67bf71c8c414cf4
steveking <steveking@b5fa89cc-b1ba-491c-ba8b-bf438c492d4e>
2010-09-12 17:29:39 +0000

    Add a simple encryption class. Note: this isn't really a secure encryption since the key and vector is stored in the assembly itself, so anybody can read them out. But it should be enough for 'bypassing users'.

---
+++ src/SimpleAES.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Data;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace EveImSync
+{
+    public class SimpleAES
+    {
+        // Change these keys for other applications
+        private byte[] Key = { 127, 15, 214, 77, 101, 74, 133, 232, 95, 125, 58, 4, 124, 12, 46, 163, 113, 101, 53, 68, 160, 42, 224, 204, 15, 126, 229, 51, 183, 118, 7, 84 };
+        private byte[] Vector = { 145, 226, 144, 251, 106, 226, 239, 188, 163, 36, 110, 124, 106, 240, 141, 113 };
+
+
+        private ICryptoTransform EncryptorTransform, DecryptorTransform;
+        private System.Text.UTF8Encoding UTFEncoder;
+
+        public SimpleAES()
+        {
+            // This is our encryption method
+            RijndaelManaged rm = new RijndaelManaged();
+
+            // Create an encryptor and a decryptor using our encryption method, key, and vector.
+            EncryptorTransform = rm.CreateEncryptor(this.Key, this.Vector);
+            DecryptorTransform = rm.CreateDecryptor(this.Key, this.Vector);
+
+            //Used to translate bytes to text and vice versa
+            UTFEncoder = new System.Text.UTF8Encoding();
+        }
+
+        ///  -------------- Two Utility Methods (not used but may be useful) -----------
+        /// Generates an encryption key.
+        static public byte[] GenerateEncryptionKey()
+        {
+            //Generate a Key.
+            RijndaelManaged rm = new RijndaelManaged();
+            rm.GenerateKey();
+            return rm.Key;
+        }
+
+        /// Generates a unique encryption vector
+        static public byte[] GenerateEncryptionVector()
+        {
+            //Generate a Vector
+            RijndaelManaged rm = new RijndaelManaged();
+            rm.GenerateIV();
+            return rm.IV;
+        }
+
+
+        /// ----------- The commonly used methods ------------------------------    
+        /// Encrypt some text and return a string suitable for passing in a URL.
+        public string EncryptToString(string TextValue)
+        {
+            return ByteArrToString(Encrypt(TextValue));
+        }
+
+        /// Encrypt some text and return an encrypted byte array.
+        public byte[] Encrypt(string TextValue)
+        {
+            // Translates our text value into a byte array.
+            Byte[] bytes = UTFEncoder.GetBytes(TextValue);
+
+            // Used to stream the data in and out of the CryptoStream.
+            MemoryStream memoryStream = new MemoryStream();
+
+
+             // We will have to write the unencrypted bytes to the stream,
+             //then read the encrypted result back from the stream.
+            #region Write the decrypted value to the encryption stream
+            CryptoStream cs = new CryptoStream(memoryStream, EncryptorTransform, CryptoStreamMode.Write);
+            cs.Write(bytes, 0, bytes.Length);
+            cs.FlushFinalBlock();
+            #endregion
+
+            #region Read encrypted value back out of the stream
+            memoryStream.Position = 0;
+            byte[] encrypted = new byte[memoryStream.Length];
+            memoryStream.Read(encrypted, 0, encrypted.Length);
+            #endregion
+
+            // Clean up.
+            cs.Close();
+            memoryStream.Close();
+
+            return encrypted;
+        }
+
+        /// The other side: Decryption methods
+        public string DecryptString(string EncryptedString)
+        {
+            return Decrypt(StrToByteArray(EncryptedString));
+        }
+
+        /// Decryption when working with byte arrays.    
+        public string Decrypt(byte[] EncryptedValue)
+        {
+            MemoryStream encryptedStream = new MemoryStream();
+            CryptoStream decryptStream = new CryptoStream(encryptedStream, DecryptorTransform, CryptoStreamMode.Write);
+            decryptStream.Write(EncryptedValue, 0, EncryptedValue.Length);
+            decryptStream.FlushFinalBlock();
+
+            encryptedStream.Position = 0;
+            Byte[] decryptedBytes = new Byte[encryptedStream.Length];
+            encryptedStream.Read(decryptedBytes, 0, decryptedBytes.Length);
+            encryptedStream.Close();
+
+            return UTFEncoder.GetString(decryptedBytes);
+        }
+
+        /// Convert a string to a byte array.  NOTE: Normally we'd create a Byte Array from a string using an ASCII encoding (like so).
+        //      System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+        //      return encoding.GetBytes(str);
+        // However, this results in character values that cannot be passed in a URL.  So, instead, I just
+        // lay out all of the byte values in a long string of numbers (three per - must pad numbers less than 100).
+        public byte[] StrToByteArray(string str)
+        {
+            if (str.Length == 0)
+                throw new Exception("Invalid string value in StrToByteArray");
+
+            byte val;
+            byte[] byteArr = new byte[str.Length / 3];
+            int i = 0;
+            int j = 0;
+            do
+            {
+                val = byte.Parse(str.Substring(i, 3));
+                byteArr[j++] = val;
+                i += 3;
+            }
+            while (i < str.Length);
+            return byteArr;
+        }
+
+        // Same comment as above.  Normally the conversion would use an ASCII encoding in the other direction:
+        //      System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+        //      return enc.GetString(byteArr);    
+        public string ByteArrToString(byte[] byteArr)
+        {
+            byte val;
+            string tempStr = "";
+            for (int i = 0; i <= byteArr.GetUpperBound(0); i++)
+            {
+                val = byteArr[i];
+                if (val < (byte)10)
+                    tempStr += "00" + val.ToString();
+                else if (val < (byte)100)
+                    tempStr += "0" + val.ToString();
+                else
+                    tempStr += val.ToString();
+            }
+            return tempStr;
+        }
+    }
+}

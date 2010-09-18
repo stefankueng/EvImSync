542177cf96afcf6aa7b10eff92e85027ea3b910e
steveking <steveking@b5fa89cc-b1ba-491c-ba8b-bf438c492d4e>
2010-09-18 07:30:03 +0000

    * remove the progress listview * add a progress bar instead * use the .NET4 client profile instead of the full .NET4 framework

---
+++ src/Enums/SyncStep.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EveImSync.Enums
+{
+    public enum SyncStep
+    {
+        ExtractNotes,
+        ParseNotes,
+        GettingImapList,
+        CalculateWhatToDo,
+        AdjustTags,
+        DownloadNotes,
+        UploadNotes
+    }
+}
--- src/MainFrm.Designer.cs
+++ src/MainFrm.Designer.cs
@@ -50,8 +50,10 @@ namespace EveImSync
             this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.startsync = new System.Windows.Forms.Button();
-            this.progressInfoList = new System.Windows.Forms.ListView();
-            this.column1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.infoText1 = new System.Windows.Forms.Label();
+            this.infoText2 = new System.Windows.Forms.Label();
+            this.progressIndicator = new System.Windows.Forms.ProgressBar();
+            this.infoText0 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -100,33 +102,60 @@ namespace EveImSync
             this.startsync.UseVisualStyleBackColor = true;
             this.startsync.Click += new System.EventHandler(this.Startsync_Click);
             // 
-            // progressInfoList
+            // infoText1
             // 
-            this.progressInfoList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
+            this.infoText1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.progressInfoList.AutoArrange = false;
-            this.progressInfoList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.column1});
-            this.progressInfoList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
-            this.progressInfoList.Location = new System.Drawing.Point(12, 56);
-            this.progressInfoList.Name = "progressInfoList";
-            this.progressInfoList.Size = new System.Drawing.Size(497, 245);
-            this.progressInfoList.TabIndex = 2;
-            this.progressInfoList.UseCompatibleStateImageBehavior = false;
-            this.progressInfoList.View = System.Windows.Forms.View.Details;
+            this.infoText1.AutoSize = true;
+            this.infoText1.Location = new System.Drawing.Point(12, 69);
+            this.infoText1.Name = "infoText1";
+            this.infoText1.Size = new System.Drawing.Size(10, 13);
+            this.infoText1.TabIndex = 3;
+            this.infoText1.Text = " ";
+            // 
+            // infoText2
+            // 
+            this.infoText2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.infoText2.AutoSize = true;
+            this.infoText2.Location = new System.Drawing.Point(12, 82);
+            this.infoText2.Name = "infoText2";
+            this.infoText2.Size = new System.Drawing.Size(10, 13);
+            this.infoText2.TabIndex = 4;
+            this.infoText2.Text = " ";
+            // 
+            // progressIndicator
+            // 
+            this.progressIndicator.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressIndicator.Location = new System.Drawing.Point(12, 98);
+            this.progressIndicator.Name = "progressIndicator";
+            this.progressIndicator.Size = new System.Drawing.Size(497, 23);
+            this.progressIndicator.TabIndex = 5;
+            // 
+            // infoText0
+            // 
+            this.infoText0.AutoSize = true;
+            this.infoText0.Location = new System.Drawing.Point(12, 56);
+            this.infoText0.Name = "infoText0";
+            this.infoText0.Size = new System.Drawing.Size(10, 13);
+            this.infoText0.TabIndex = 6;
+            this.infoText0.Text = " ";
             // 
             // MainFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(521, 313);
-            this.Controls.Add(this.progressInfoList);
+            this.ClientSize = new System.Drawing.Size(521, 133);
+            this.Controls.Add(this.infoText0);
+            this.Controls.Add(this.progressIndicator);
+            this.Controls.Add(this.infoText2);
+            this.Controls.Add(this.infoText1);
             this.Controls.Add(this.startsync);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainFrm";
-            this.Text = "Form1";
+            this.Text = "EvImSync";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -141,8 +170,10 @@ namespace EveImSync
         private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.Button startsync;
-        private System.Windows.Forms.ListView progressInfoList;
-        private System.Windows.Forms.ColumnHeader column1;
+        private System.Windows.Forms.Label infoText1;
+        private System.Windows.Forms.Label infoText2;
+        private System.Windows.Forms.ProgressBar progressIndicator;
+        private System.Windows.Forms.Label infoText0;
     }
 }
 

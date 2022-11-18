74b1537da1d029f5264e57ab9f3e47b248108009
Stefan Küng <stefankueng@gmail.com>
2022-11-18 21:21:13 +0100

    update framework

--- src/InterIMAP/InterIMAP-Async/InterIMAP/Common/Data/Mailbox.cs
+++ src/InterIMAP/InterIMAP-Async/InterIMAP/Common/Data/Mailbox.cs
@@ -1,13 +1,15 @@
-﻿namespace InterIMAP.Common.Data {
-    
-    
-    public partial class Mailbox {
-        
-    
+﻿namespace EveImSync.InterIMAP.InterIMAP_Async.InterIMAP.Common.Data
+{
+
+
+    public partial class Mailbox
+    {
+
+
         partial class MessageDataTable
         {
         }
-    
+
         partial class ConfigDataTable
         {
         }
